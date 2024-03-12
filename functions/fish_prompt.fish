7d3032170a8b76ce33a8ca9e7e55674d3e968211
matthewq <quminzhi@gmail.com>
2024-03-12 15:22:05 -0700

    Changed prefix name

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,91 +1,91 @@
-set __toaster_color_red AF0000
-set __toaster_color_orange FD971F
-set __toaster_color_blue 6EC9DD
-set __toaster_color_green A6E22E
-set __toaster_color_yellow E6DB7E
-set __toaster_color_pink F92672
-set __toaster_color_grey 554F48
-set __toaster_color_light_grey BCBCBC
-set __toaster_color_white F1F1F1
-set __toaster_color_purple AF5FFF
-set __toaster_color_lilac AE81FF
+set __fshthm_color_red AF0000
+set __fshthm_color_orange FD971F
+set __fshthm_color_blue 6EC9DD
+set __fshthm_color_green A6E22E
+set __fshthm_color_yellow E6DB7E
+set __fshthm_color_pink F92672
+set __fshthm_color_grey 554F48
+set __fshthm_color_light_grey BCBCBC
+set __fshthm_color_white F1F1F1
+set __fshthm_color_purple AF5FFF
+set __fshthm_color_lilac AE81FF
 
-function __toaster_color_echo
+function __fshthm_color_echo
   set_color $argv[1]
   if test (count $argv) -eq 2
     echo -n $argv[2]
   end
 end
 
-function __toaster_prompt_hostname
+function __fshthm_prompt_hostname
   set -l host (hostname -s)
   set -l user (whoami)
 
   if [ "$user" = "root" ]
-    __toaster_color_echo $__toaster_color_red $user
-	  __toaster_color_echo $__toaster_color_purple '@'$host' '
+    __fshthm_color_echo $__fshthm_color_red $user
+	  __fshthm_color_echo $__fshthm_color_purple '@'$host' '
   else
-    __toaster_color_echo $__toaster_color_blue ' '$user
-	  __toaster_color_echo $__toaster_color_orange ' [@'$host'] '
+    __fshthm_color_echo $__fshthm_color_blue ' '$user
+	  __fshthm_color_echo $__fshthm_color_orange ' [@'$host'] '
   end
 end
 
-function __toaster_git_status_codes
+function __fshthm_git_status_codes
   echo (git status --porcelain 2> /dev/null | sed -E 's/(^.{3}).*/\1/' | tr -d ' \n')
 end
 
-function __toaster_git_branch_name
+function __fshthm_git_branch_name
   echo (git rev-parse --abbrev-ref HEAD 2> /dev/null)
 end
 
-function __toaster_rainbow
+function __fshthm_rainbow
   if echo $argv[1] | grep -q -e $argv[3]
-    __toaster_color_echo $argv[2] "彡ミ"
+    __fshthm_color_echo $argv[2] "彡ミ"
   end
 end
 
-function __toaster_git_status_icons
-  set -l git_status (__toaster_git_status_codes)
+function __fshthm_git_status_icons
+  set -l git_status (__fshthm_git_status_codes)
 
-  __toaster_rainbow $git_status $__toaster_color_pink 'D'
-  __toaster_rainbow $git_status $__toaster_color_orange 'R'
-  __toaster_rainbow $git_status $__toaster_color_white 'C'
-  __toaster_rainbow $git_status $__toaster_color_green 'A'
-  __toaster_rainbow $git_status $__toaster_color_blue 'U'
-  __toaster_rainbow $git_status $__toaster_color_lilac 'M'
-  __toaster_rainbow $git_status $__toaster_color_grey '?'
+  __fshthm_rainbow $git_status $__fshthm_color_pink 'D'
+  __fshthm_rainbow $git_status $__fshthm_color_orange 'R'
+  __fshthm_rainbow $git_status $__fshthm_color_white 'C'
+  __fshthm_rainbow $git_status $__fshthm_color_green 'A'
+  __fshthm_rainbow $git_status $__fshthm_color_blue 'U'
+  __fshthm_rainbow $git_status $__fshthm_color_lilac 'M'
+  __fshthm_rainbow $git_status $__fshthm_color_grey '?'
 end
 
-function __toaster_git_status
+function __fshthm_git_status
   # In git
-  if test -n (__toaster_git_branch_name)
+  if test -n (__fshthm_git_branch_name)
 
-    __toaster_color_echo $__toaster_color_blue " git"
-    __toaster_color_echo $__toaster_color_white ":"(__toaster_git_branch_name)
+    __fshthm_color_echo $__fshthm_color_blue " git"
+    __fshthm_color_echo $__fshthm_color_white ":"(__fshthm_git_branch_name)
 
-    if test -n (__toaster_git_status_codes)
-      __toaster_color_echo $__toaster_color_pink ' ●'
-      __toaster_color_echo $__toaster_color_white ' [^._.^]ﾉ'
-      __toaster_git_status_icons
+    if test -n (__fshthm_git_status_codes)
+      __fshthm_color_echo $__fshthm_color_pink ' ●'
+      __fshthm_color_echo $__fshthm_color_white ' [^._.^]ﾉ'
+      __fshthm_git_status_icons
     else
-      __toaster_color_echo $__toaster_color_green ' ○'
+      __fshthm_color_echo $__fshthm_color_green ' ○'
     end
   end
 end
 
 function fish_prompt
-  __toaster_color_echo $__toaster_color_blue "(◕‿◕)➜"
-  __toaster_prompt_hostname
-  __toaster_color_echo $__toaster_color_light_grey (prompt_pwd)
-  __toaster_git_status
+  __fshthm_color_echo $__fshthm_color_blue "(◕‿◕)➜"
+  __fshthm_prompt_hostname
+  __fshthm_color_echo $__fshthm_color_light_grey (prompt_pwd)
+  __fshthm_git_status
   echo
 
   set -l uid (id -u $USER)
   if test $uid -eq '0'
-    __toaster_color_echo $__toaster_color_yellow "❯ "
+    __fshthm_color_echo $__fshthm_color_yellow "❯ "
   else
-  	__toaster_color_echo $__toaster_color_blue "❯" 
-    __toaster_color_echo $__toaster_color_yellow "❯"
-    __toaster_color_echo $__toaster_color_light_grey "❯ "
+  	__fshthm_color_echo $__fshthm_color_blue "❯" 
+    __fshthm_color_echo $__fshthm_color_yellow "❯"
+    __fshthm_color_echo $__fshthm_color_light_grey "❯ "
   end
 end
